40eb85100129c1791369604fd9996048168582d0
Fredocg012 <fredo012@comunidad.unam.mx>
2022-02-18 01:17:08 -0600

    Proyecto del dia 1

---
+++ day-1/Proyect_Day_1.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+ #Band name generator
+ 
+ #1.- Create a greting for your program.
+ #2.- Ask the user for the city that they grew up.
+ #3.- Ask the user for the name of a pet.
+ #4.- Combine the name of their city and pet and show them their name band.
+ #5.- Make shure the input cursor shows on a new line, see the example.
+ 
+ 
+print("Bienvenido al generador de nombre de bandas de rock")
+ciudad = input("¿En que ciudad creció? ")
+mascota = input("¿Cuál es el nombre de su mascota? ")
+print(ciudad + " de " + mascota)
+ 
