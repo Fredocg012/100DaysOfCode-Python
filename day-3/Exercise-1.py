99cc82fe8100a8277071ba2e983fd0cfbde5cd89
Fredocg012 <fredo012@comunidad.unam.mx>
2022-02-23 23:50:08 -0600

    Número par o impar

---
+++ day-3/Exercise-1.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 23 23:31:20 2022
+
+@author: pokem
+"""
+
+# Odd or even (Número par o impar)
+
+numero = float(input("Dame un numero: "))
+
+
+
+if((numero % 2.0) == 0):
+    print("El número es par")
+    
+else:
+    print("El número es impar")
+    
+
+    
